863a01d01e776f62971f74747fe99266da4f3c8d
Johan Hermansson <johan.hermansson@medsci.uu.se>
2018-01-15 09:11:57 +0100

    Fix wrong md5sum output path

--- archive_verify/workers.py
+++ archive_verify/workers.py
@@ -96,9 +96,8 @@ def verify_archive(archive, host, description, config):
     if not download_ok:
         return {"state": "error", "msg": "failed to properly download archive from pdc", "path": dest}
     else:
-        check_dir = os.path.join(dest_root, archive)
-        verified_ok = compare_md5sum(check_dir)
-        output_file = "{}/compare_md5sum.out".format(check_dir)
+        verified_ok = compare_md5sum(dest)
+        output_file = "{}/compare_md5sum.out".format(dest)
 
         if verified_ok:
             return {"state": "done", "path": output_file, "msg": "sucessfully verified archive md5sums"}
